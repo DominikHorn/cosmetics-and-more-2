58dd4c184a149e889bf7d143263a380345669c2c
Dominik Horn <dominik.horn@tum.de>
2020-09-19 11:51:43 +0200

    set dark/light theme based on user preference

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -1,37 +1,25 @@
-import {
-  createMuiTheme,
-  CssBaseline,
-  MuiThemeProvider,
-  ThemeOptions,
-  Button,
-} from "@material-ui/core";
+import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import * as React from "react";
-import { useState } from "react";
 import { hot } from "react-hot-loader";
 
 import { ErrorBoundaryComponent } from "@cosmetics-and-more/components";
 import { darkTheme, lightTheme } from "./theme";
 
-const useDarkMode = (): [ThemeOptions, () => void] => {
-  const [theme, setTheme] = useState(lightTheme);
-
-  const toggleTheme = (): void => {
-    setTheme(theme.palette!.type === "light" ? darkTheme : lightTheme);
-  };
-  return [theme, toggleTheme];
-};
-
 const AppComponent = (): React.ReactElement => {
-  const [theme, toggleDarkMode] = useDarkMode();
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = React.useMemo(
+    () => createMuiTheme(prefersDarkMode ? darkTheme : lightTheme),
+    [prefersDarkMode]
+  );
 
   return (
-    <MuiThemeProvider theme={createMuiTheme(theme)}>
+    <ThemeProvider theme={createMuiTheme(theme)}>
       <CssBaseline />
       <ErrorBoundaryComponent>
         <div>hier könnte ihre Werbung stehen</div>
-        <Button onClick={toggleDarkMode}>Toggle</Button>
       </ErrorBoundaryComponent>
-    </MuiThemeProvider>
+    </ThemeProvider>
   );
 };
 
