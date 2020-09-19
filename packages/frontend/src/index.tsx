fa9a4f104b8abbbc248e8cc83e595c83cff409fd
Dominik Horn <dominik.horn@tum.de>
2020-09-19 19:40:33 +0200

    add smooth-scrolling to all browsers via polyfill

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import smoothscroll from "smoothscroll-polyfill";
 
 import { App } from "./app";
 
+smoothscroll.polyfill();
 ReactDOM.render(<App />, document.getElementById("root"));
