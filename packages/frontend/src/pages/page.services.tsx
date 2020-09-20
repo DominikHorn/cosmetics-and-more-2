7a38928fb51aa9cae48b9abab1e3f3d3b822d1a4
Dominik Horn <dominik.horn@tum.de>
2020-09-20 15:52:29 +0200

    add hair services to services page

--- packages/frontend/src/pages/page.services.tsx
+++ packages/frontend/src/pages/page.services.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { route } from "../routing";
 import RoomServiceIcon from "@material-ui/icons/RoomService";
 import { Grid } from "@material-ui/core";
-import { cosmeticServices } from "./content";
+import { cosmeticServices, hairServices } from "./content";
 import { ServiceComponent } from "@cosmetics-and-more/components";
 import { DEFAULT_ANIMATION_DELAY } from "@cosmetics-and-more/types";
 
@@ -16,7 +16,7 @@ export class ServicesPage extends React.PureComponent {
   render() {
     return (
       <Grid container spacing={2}>
-        {cosmeticServices.map((service, i) => (
+        {[...cosmeticServices, ...hairServices].map((service, i) => (
           <Grid item key={i} xs={12} sm={6} md={4} xl={2}>
             <ServiceComponent
               {...service}
