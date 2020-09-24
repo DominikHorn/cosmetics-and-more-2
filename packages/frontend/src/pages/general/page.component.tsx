cb8097911a6376b5e37c79fbb59a6a7d7edb27eb
Dominik Horn <dominik.horn@tum.de>
2020-09-24 18:39:03 +0200

    base isDesktop detection on theme breakpoints

--- packages/frontend/src/pages/general/page.component.tsx
+++ packages/frontend/src/pages/general/page.component.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import { routes, IRoute } from "../../routing";
-import { useMediaQuery } from "@material-ui/core";
+import { useMediaQuery, useTheme } from "@material-ui/core";
 import { INavigationProps } from "./types";
 import { DesktopPage } from "./page.component.desktop";
 import { MobilePage } from "./page.component.mobile";
 
 export const PageComponent = () => {
-  // TODO: generalize mobile detection logic
-  const isDesktop = useMediaQuery("only screen and (min-width: 768px)", {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("sm"), {
     // disabling server side rendering prevents double page render on load
     // which in turn prevents false negatives for 'isDesktop' on initial page
     // load
