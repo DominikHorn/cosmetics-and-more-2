327cee8dbc97bd5f070c2875f3643a2bf18019ef
Dominik Horn <dominik.horn@tum.de>
2020-09-26 15:58:48 +0200

    remove theming from app.tsx
    theming will be done on a per route basis in the future

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -1,30 +1,18 @@
-import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { CssBaseline } from "@material-ui/core";
 import React from "react";
 import { hot } from "react-hot-loader";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { ErrorBoundaryComponent } from "@cosmetics-and-more/components";
-import { darkTheme, lightTheme } from "./theme";
 import { PageComponent } from "./pages";
 
-const AppComponent = (): React.ReactElement => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const theme = React.useMemo(
-    () => createMuiTheme(prefersDarkMode ? darkTheme : lightTheme),
-    [prefersDarkMode]
-  );
-
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <ThemeProvider theme={createMuiTheme(theme)}>
-        <CssBaseline />
-        <ErrorBoundaryComponent>
-          <PageComponent />
-        </ErrorBoundaryComponent>
-      </ThemeProvider>
-    </Router>
-  );
-};
+const AppComponent = (): React.ReactElement => (
+  <Router basename={process.env.PUBLIC_URL}>
+    <CssBaseline />
+    <ErrorBoundaryComponent>
+      <PageComponent />
+    </ErrorBoundaryComponent>
+  </Router>
+);
 
 export const App = hot(module)(AppComponent);
