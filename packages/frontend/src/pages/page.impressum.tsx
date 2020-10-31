47c5a3850f4144a369576e401537b4dda4059911
Dominik Horn <dominik.horn@tum.de>
2020-10-31 23:02:27 +0100

    stub impressum page

--- packages/frontend/src/pages/page.impressum.tsx
+++ packages/frontend/src/pages/page.impressum.tsx
@@ -1,2 +1,19 @@
-// TODO: attribute
-// Foto von Shiny Diamond von Pexels
+import React from "react";
+import { route } from "../routing";
+import PrivacyIcon from "@material-ui/icons/Lock";
+import { Alert } from "@material-ui/lab";
+
+@route({
+  displayName: "Impressum & Datenschutz",
+  path: "/impressum",
+  icon: <PrivacyIcon />,
+  prio: 5,
+})
+export class ImpressumPage extends React.PureComponent {
+  render() {
+    // TODO: attributieren von Bild:
+    // Foto von Shiny Diamond von Pexels
+
+    return <Alert severity={"error"}>Unimplemented</Alert>;
+  }
+}
