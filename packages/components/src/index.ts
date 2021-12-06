e5d0d2da512a06cf0d59e3480f66136329846e95
Dominik Horn <dominik.horn@tum.de>
2021-12-06 18:07:41 +0100

    add corona info banner

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,4 +1,5 @@
+export * from "./contactcard.component";
 export * from "./errorboundary.component";
+export * from "./infobanner.component";
 export * from "./loremipsum.component";
 export * from "./service.component";
-export * from "./contactcard.component";
---
+++ packages/components/src/infobanner.component.tsx
@@ -0,0 +1,54 @@
+import React, { useCallback, useState } from "react";
+import {
+  makeStyles,
+  Snackbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+
+export interface IInfoBannerComponentProps {
+  readonly message: string;
+  readonly severity?: "error" | "info";
+}
+
+const useStyles = makeStyles({
+  desktopBar: {
+    marginTop: "40px",
+    boxShadow: "5px 5px 20px gray",
+  },
+  mobileBar: {
+    marginBottom: "10px",
+    boxShadow: "5px 5px 20px gray",
+  },
+});
+
+export const InfoBannerComponent = (props: IInfoBannerComponentProps) => {
+  const classes = useStyles(props);
+  const [open, setOpen] = useState(true);
+  const handleClose = useCallback(() => setOpen(false), [setOpen]);
+  const theme = useTheme();
+
+  const isDesktop = useMediaQuery(theme.breakpoints.up("sm"), {
+    // disabling server side rendering prevents double page render on load
+    // which in turn prevents false negatives for 'isDesktop' on initial page
+    // load
+    noSsr: true,
+  });
+
+  return (
+    <Snackbar
+      open={open}
+      anchorOrigin={{
+        vertical: isDesktop ? "top" : "bottom",
+        horizontal: "center",
+      }}
+      className={isDesktop ? classes.desktopBar : classes.mobileBar}
+    >
+      <Alert severity={props.severity || "error"} onClose={handleClose}>
+        <Typography variant="h5">{props.message}</Typography>
+      </Alert>
+    </Snackbar>
+  );
+};
