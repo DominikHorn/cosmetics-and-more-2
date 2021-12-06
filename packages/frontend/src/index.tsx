df5aa19b54554010bdf13df1c44ae1b214d2a900
Dominik Horn <dominik.horn@tum.de>
2021-12-06 16:50:27 +0100

    update mapkit token

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -6,7 +6,7 @@ import { App } from "./app";
 
 // Initialize mapkit-js
 const mapkitJWT =
-  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjM4QkdOTlE5NjgifQ.eyJpc3MiOiJLNjZUVzdGUk4zIiwiaWF0IjoxNjAxMTQwMzYyLjE3MiwiZXhwIjoxNjMyNjc2MzYyLjE3Mn0.62zCnRGk8wM-TW_iBxao10YIQVW-GcGmUD-DjTeA-6sKyzRnZq11tMVAgAmVcjqkIrBq8Hc9m1hJeET5IkZMnA";
+  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkFKNFQ0VTJSTkwifQ.eyJpc3MiOiJLNjZUVzdGUk4zIiwiaWF0IjoxNjM4ODA1NTk1LjY5OSwiZXhwIjoxNjcwMzQxNTk1LjY5OX0.-t3RhnFpQb8WMR_dUuHKckXEjK7kETLdxtMgg1CiKHy3iedqsuqjfsVTWu3Oc90QV6MzWaSaUxcP8NLIY4_6Gg";
 (window as any).mapkit.init({
   authorizationCallback: (done: (token: string) => void) => done(mapkitJWT),
 });
