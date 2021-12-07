0bf1df25bc35ce283465ec784f78921f03f1d1a8
Dominik Horn <dominik.horn@tum.de>
2021-12-07 18:58:43 +0100

    add seasonal snow effect to webpage

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -4,13 +4,22 @@ import { hot } from "react-hot-loader";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ErrorBoundaryComponent } from "@cosmetics-and-more/components";
 import { PageComponent } from "./pages";
+import Snowfall from "react-snowfall";
 
-const AppComponent = (): React.ReactElement => (
-  <Router>
-    <ErrorBoundaryComponent>
-      <PageComponent />
-    </ErrorBoundaryComponent>
-  </Router>
-);
+const AppComponent = (): React.ReactElement => {
+  const now = new Date();
+  const month = now.getUTCMonth() + 1;
+  const shouldSnow = month > 11 || month < 2;
+
+  return (
+    <Router>
+      <ErrorBoundaryComponent>
+        <PageComponent />
+      </ErrorBoundaryComponent>
+
+      {shouldSnow && <Snowfall />}
+    </Router>
+  );
+};
 
 export const App = hot(module)(AppComponent);
