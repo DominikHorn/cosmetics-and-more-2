b1e0976b954c67fbe8898d770063e950f902d1d3
Dominik Horn <dominik.horn@tum.de>
2021-12-07 20:05:02 +0100

    improve seasonal snow effect

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { hot } from "react-hot-loader";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { ErrorBoundaryComponent } from "@cosmetics-and-more/components";
 import { PageComponent } from "./pages";
-import Snowfall from "react-snowfall";
+import Snowflakes from "magic-snowflakes";
 
 const AppComponent = (): React.ReactElement => {
   const now = new Date();
   const month = now.getUTCMonth() + 1;
   const shouldSnow = month > 11 || month < 2;
 
+  const [snowflakes] = useState(
+    new Snowflakes({
+      color: "#85dede",
+      count: 100,
+      minOpacity: 0.8,
+      maxOpacity: 1.0,
+      minSize: 15,
+      maxSize: 30,
+      rotation: true,
+      speed: 1,
+      wind: true,
+    })
+  );
+  useEffect(() => (shouldSnow ? snowflakes.start() : snowflakes.stop()), [
+    shouldSnow,
+  ]);
+
   return (
     <Router>
       <ErrorBoundaryComponent>
         <PageComponent />
       </ErrorBoundaryComponent>
-
-      {shouldSnow && <Snowfall />}
     </Router>
   );
 };
