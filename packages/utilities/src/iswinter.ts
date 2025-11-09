d370ef1b49c02c98a0e0410702f70c0a7850bf65
Dominik Horn <dominik.horn@tum.de>
2025-11-09 12:43:32 +0100

    Start snowing in November

--- packages/utilities/src/iswinter.ts
+++ packages/utilities/src/iswinter.ts
@@ -1,7 +1,7 @@
 export function isWinter(): boolean {
   const now = new Date();
   const month = now.getUTCMonth() + 1;
-  return month > 11 || month < 2;
+  return month >= 11 || month < 2;
 }
 
 export function isChristmas(): boolean {
